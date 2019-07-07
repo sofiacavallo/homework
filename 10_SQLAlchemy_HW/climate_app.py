ee98883b350d9a16f08cb4a9c74f5187cb8517d7
sofiacavallo <sofia.c.cavallo@gmail.com>
2019-07-07 13:34:05 -0400

    Modifications to Python file (re-added imports)

--- 10_SQLAlchemy_HW/climate_app.py
+++ 10_SQLAlchemy_HW/climate_app.py
@@ -1,5 +1,23 @@
-# Import Flask
+# Imports from IPYNB file
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+session = Session(engine)
+
 
 # Create an app, being sure to pass __name__ (boilerplate)
 app = Flask(__name__)
